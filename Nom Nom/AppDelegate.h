916cb74254a41c9178628cc617b60c1ab5642bbc
Garrett Votaw <garrettvotaw@garretts-macbook-pro.local>
2018-03-10 18:41:24 -0600

    Initial Commit

---
+++ Nom Nom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nom Nom
+//
+//  Created by Garrett Votaw on 3/10/18.
+//  Copyright © 2018 Garrett Votaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nom Nom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nom Nom
+//
+//  Created by Garrett Votaw on 3/10/18.
+//  Copyright © 2018 Garrett Votaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
